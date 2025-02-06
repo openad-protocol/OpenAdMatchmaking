c3372298e6003427dc49ee84a2644b9a6046c227
cloud ye <email>
2025-02-06 10:09:42 +0800

    * 变更为UV

--- scripts/redis_data.lua
+++ scripts/redis_data.lua
@@ -569,7 +569,7 @@ end
 
 function RedisData:getEventId(data,zoneId,publisherId)
     -- 全局规则判断
-    local globalDayIpPvNumber = self:getIpAddrPv("loginfo",data.ip_address)
+    local globalDayIpPvNumber = self:getIpAddrUv("loginfo",data.ip_address)
     local globalThreeNumber = self:getThreeIpPv("loginfo",data.ip_address)
     if globalDayIpPvNumber ~= nil and globalThreeNumber ~=nil then
         -- 取IP取则
