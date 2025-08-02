d8bac8693c8ee1bd3d42eedebbac737dbaa2498a
cloud ye <jxncyjq@163.com>
2025-08-02 22:18:19 +0800

    Update redis_data.lua

--- scripts/redis_data.lua
+++ scripts/redis_data.lua
@@ -751,29 +751,29 @@ function RedisData:getEventId(data,zoneId,publisherId)
         ngx.log(ngx.DEBUG,string.format("eventId:%s, singleNumberPv:%s,totalMaxUv:%s,totalMaxPv:%s",
          eventId,singleNumberPv,totalMaxUv,totalMaxPv))
         -- UV判断
-        if tbEvent.totalMaxUv ~=nil and totalMaxUv >= tbEvent.singleMaxUv then
+        if tbEvent.totalMaxUv ~=nil and (totalMaxUv or 0) >= tbEvent.singleMaxUv then
             ngx.log(ngx.DEBUG,string.format("event id:%s, totalMaxUv:%s,tbEvent.totalMaxUv:%s, traceId:%s",
                 eventId,totalMaxUv,tbEvent.totalMaxUv,data.traceId))
             bNotFindEvent = false
             goto continue
         end
         -- PV判断
-        if tbEvent.totalMaxPv ~= nil and tbEvent.totalMaxPv ~= cjson.null and tbEvent.totalMaxPv ~= -1 and totalMaxPv >= tbEvent.totalMaxPv then
+        if tbEvent.totalMaxPv ~= nil and tbEvent.totalMaxPv ~= cjson.null and tbEvent.totalMaxPv ~= -1 and (totalMaxPv or 0) >= tbEvent.totalMaxPv then
             ngx.log(ngx.DEBUG,string.format("event id:%s, singleMaxpv:%s,tbEvent.totalMaxPv:%s,traceId:%s",
                 eventId,totalMaxPv,tbEvent.totalMaxPv,data.traceId))
             bNotFindEvent = false
             goto continue
         end
         -- 单用户PV判断
-        if tbEvent.singleMaxpv ~= nil and tbEvent.singleMaxpv ~= cjson.null and tbEvent.singleMaxpv ~= -1 and singleNumberPv >= tbEvent.singleMaxpv then
+        if tbEvent.singleMaxpv ~= nil and tbEvent.singleMaxpv ~= cjson.null and tbEvent.singleMaxpv ~= -1 and (singleNumberPv or 0) >= tbEvent.singleMaxpv then
             ngx.log(ngx.DEBUG,string.format("event id:%s, singleMaxpv:%s,singleNumber:%s",
                 eventId,tbEvent.singleMaxpv,singleNumberPv))
             bNotFindEvent = false
             goto continue
         end
         -- 30秒判断
-        if tbEvent.threeSecLimit ~=nil and threeZonePvNumber >= tbEvent.threeSecLimit then
-            ngx.log(ngx.DEBUG,string.format("event id:%s,three sec pv is more than 30:%s three sec limit:%s",eventId,threeSecNumber,tbEvent.threeSecLimit))
+        if tbEvent.threeSecLimit ~=nil and (threeZonePvNumber or 0) >= tbEvent.threeSecLimit then
+            ngx.log(ngx.DEBUG,string.format("event id:%s,three sec pv is more than 30:%s three sec limit:%s",eventId,(threeZonePvNumber or 0),tbEvent.threeSecLimit))
             bNotFindEvent = false
             goto continue
         end
